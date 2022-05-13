954730a68879f0d12b13f712787343d4a362617b
Bello Tolulope <belloanuoluwapo5@gmail.com>
2022-05-13 22:40:28 +0100

    added required fields

--- script.js
+++ script.js
@@ -5,19 +5,27 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
 
+//  showing input error
 function showError(input, message) {
     const formControl = input.parentElement;
+    // console.log(formControl)
     formControl.className = 'form-control error';
-    const small = input.nextElementSibling; // update here 
+    const small = formControl.querySelector('small'); // update here 
     small.innerText = message;
 }
+// // showing input success 
 function showSuccess(input) {
     const formControl = input.parentElement;
     formControl.className = 'form-control success';
 }
+// showing email validation
+function validEmail(email){
+    const re =/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  return re.test (String(email).toLowerCase());
+}
 
 // adding the eventlistners
-form.addEventListener(submit, function(e) {
+form.addEventListener('submit', function(e) {
     e.preventDefault();
 
     if (username.value === '') {
@@ -25,6 +33,28 @@ form.addEventListener(submit, function(e) {
 
     } else {
         showSuccess(username);
+    }
+
+    if (email.value === '') {
+        showError(email, "email is required");
+
+    } else if(!validEmail(email.value)){
+        showError(email, "email is not valid");
+
+    }else {
+        showSuccess(email);
+    }
+    if (password.value === '') {
+        showError(password, "password is required");
+
+    } else {
+        showSuccess(password);
+    }
+    if (password2.value === '') {
+        showError(password2, "password2 is required");
+
+    } else {
+        showSuccess(password2);
     }
 });
 
