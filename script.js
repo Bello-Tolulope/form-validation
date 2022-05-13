063806a4058e4426b3d8476903e5ed850aa04e26
Bello Tolulope <belloanuoluwapo5@gmail.com>
2022-05-13 09:30:00 +0100

    added event listeners

---
+++ script.js
@@ -0,0 +1,47 @@
+// pulling in all the dom element needed
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = input.nextElementSibling; // update here 
+    small.innerText = message;
+}
+// adding the eventlistners
+form.addEventListener(submit, function(e) {
+    e.preventDefault();
+
+    if (username.value === '') {
+        showError(username, "username is required");
+
+    } else {
+        showSuccess(username);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// // showing input error
+// function showError(input,_message){ 
+
+//     const formControl =input.parentElement;
+// }
+// 
+newFunction();
+
+function newFunction() {
+    
+}
